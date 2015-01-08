0c24be04197f73c59fe3e5219497b702a8e0aa9d
Neil E. Pearson <neil@helium.net.au>
2015-01-08 21:27:41 +1100

    better

--- lib/statements/reader/common/st_george.rb
+++ lib/statements/reader/common/st_george.rb
@@ -1,6 +1,8 @@
 module StGeorge
+  ABNS = %w[33007457141 92055513070]
+
   def st_george?
-    include?('St.George') && (include?('ABN 33 007 457 141') || include?('ABN 92 055 513 070'))
+    document =~ /\bSt\. *George\b/ && document =~ /\bABN *((?:\d *){11})/ && ABNS.include?($1.delete ' ')
   end
 
   def years
